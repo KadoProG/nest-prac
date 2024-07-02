b6591fe856200be3510fc7c408af98dce5f55a17
KadoProG <splatrain@gmail.com>
2024-07-02 10:04:30 +0900

    add ToDoアプリの基盤を作成

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { Todo } from './todo.entity';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private readonly todosService: TodosService) {}
+
+  @Get()
+  findAll(): Todo[] {
+    return this.todosService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): Todo {
+    return this.todosService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() todo: Partial<Todo>): Todo {
+    return this.todosService.create(todo);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() todo: Partial<Todo>,
+  ): Todo {
+    return this.todosService.update(id, todo);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number): void {
+    this.todosService.delete(id);
+  }
+}
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Todo } from './todo.entity';
 
 @Injectable()
-export class TodosService {}
+export class TodosService {
+  private todos: Todo[] = [];
+  private idCounter = 1;
+
+  findAll(): Todo[] {
+    return this.todos;
+  }
+
+  findOne(id: number): Todo {
+    const todo = this.todos.find((todo) => todo.id === id);
+    if (!todo) {
+      throw new NotFoundException(`Todo item with ID ${id} not found`);
+    }
+    return todo;
+  }
+
+  create(todo: Partial<Todo>): Todo {
+    const newTodo = { ...todo, id: this.idCounter++, done: false } as Todo;
+    this.todos.push(newTodo);
+    return newTodo;
+  }
+
+  update(id: number, updatedTodo: Partial<Todo>): Todo {
+    const todo = this.findOne(id);
+    if (!todo) {
+      throw new NotFoundException(`Todo item with ID ${id} not found`);
+    }
+    Object.assign(todo, updatedTodo);
+    return todo;
+  }
+
+  delete(id: number): void {
+    const todoIndex = this.todos.findIndex((todo) => todo.id === id);
+    if (todoIndex === -1) {
+      throw new NotFoundException(`Todo item with ID ${id} not found`);
+    }
+    this.todos.splice(todoIndex, 1);
+  }
+}
