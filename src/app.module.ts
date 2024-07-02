0bc42cca97e1be3e654cd8ab25ab41881cdcbc9a
KadoProG <splatrain@gmail.com>
2024-07-02 10:27:35 +0900

    add .env環境変数の構築

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TodosModule } from './todos/todos.module';
 import { Todo } from './todos/todo.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'todos.db',
-      entities: [Todo],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true, // グローバルモジュールとして利用
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite', // SQLiteを使用する場合の設定
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [Todo],
+        synchronize: configService.get<boolean>('DATABASE_SYNCHRONIZE'),
+      }),
     }),
     TodosModule,
   ],
